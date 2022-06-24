47c9c6b27aaf69980c1116ecca02b08da3a81182
angelique-df <faye.angelique.d@gmail.com>
2022-06-24 20:01:10 +0000

    Fix error 503 on heroku

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ const http = require("http");
 const app = require("./app");
 const server = http.createServer(app);
 
-server.listen(5000, () => {
+server.listen(process.env.PORT || 5000, () => {
 	console.log(`Server running on port ${5000}`);
 });
