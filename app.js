aa02c6eb368a195ae57788179c816fd269950cc4
angelique-df <faye.angelique.d@gmail.com>
2022-07-16 20:26:05 +0000

    Install `cors`
    Allow the frontend to send requests to this express app

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require("express");
 const app = express();
 const helmet = require("helmet");
 const dns = require("dns");
+const cors = require("cors");
 
 app.use(helmet());
+app.use(cors());
 
 module.exports = app;
