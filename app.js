73f7c593dfb666324cb6fe0b76b6aa7e13aa3c8d
angelique-df <faye.angelique.d@gmail.com>
2022-07-16 20:28:50 +0000

    Add middleware to validate IP addresses
    Sends a response if the user entered a valid IP address in the form. Otherwise, it moves on to the next middleware.
    
    IP addresses are validated with the `net` package.

--- app.js
+++ app.js
@@ -2,9 +2,23 @@ const express = require("express");
 const app = express();
 const helmet = require("helmet");
 const dns = require("dns");
+const net = require("net");
 const cors = require("cors");
 
 app.use(helmet());
 app.use(cors());
+
+app.get("/api/", async (req, res, next) => {
+	const search = req.query.search;
+	const isIpAddress = net.isIP(search);
+	if (isIpAddress) {
+		console.log(`This is an IPv${isIpAddress} address.`);
+		return res.status(400).json({
+			message: `You have searched an IPv${isIpAddress} address.`,
+			result: null,
+		});
+	}
+	next();
+});
 
 module.exports = app;
