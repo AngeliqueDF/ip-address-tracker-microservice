7d0477ea75af5d404231cb886f4301a15a5f2fa3
angelique-df <faye.angelique.d@gmail.com>
2022-07-19 18:32:28 +0000

    Fix typo

--- app.js
+++ app.js
@@ -58,7 +58,7 @@ app.get("/api/", async (req, res, next) => {
 app.use((err, req, res, next) => {
 	return res.status(404).json({
 		message:
-			"Could not find information from the search provided. Make sure to enter a valid the domain name or IP address.",
+			"Could not find information from the search provided. Make sure to enter a valid domain name or IP address.",
 		result: null,
 	});
 });
