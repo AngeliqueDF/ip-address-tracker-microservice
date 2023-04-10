63f8450f550b29681c2ac9f1d022fe2ee3ccfc32
angelique-df <faye.angelique.d@gmail.com>
2023-04-10 20:12:37 +0000

    Update URLSearched.ts

--- src/URLSearched.ts
+++ src/URLSearched.ts
@@ -2,23 +2,32 @@ const dns = require("dns");
 
 export class URLSearched {
 	private search: string;
+	private parsedSearch: URL;
 
 	constructor(search: string) {
 		this.search = this.addProtocol(search) ? search : `https://${search}`;
+		this.parsedSearch = new URL(this.search);
 	}
 
 	addProtocol(search: string): boolean {
 		return /^(https?:\/\/)/.test(search);
 	}
 
+	/**
+	 * Pre-condition: search term must be a valid url indicating the protocol.
+	 * @returns The IP address matching the domain name.
+	 */
 	getIpAddress(): Promise<string> {
 		return new Promise<string>((resolve) => {
-			const parsedSearch = new URL(this.search);
+			// const parsedSearch = new URL(this.search);
 
 			dns.resolve(
-				parsedSearch.hostname,
+				this.parsedSearch.hostname,
 				async (err: NodeJS.ErrnoException | null, addresses: string[]) => {
-					if (err) throw new Error(err.message);
+					if (err) {
+						console.trace(err);
+						throw new Error(err.message);
+					}
 					if (addresses) {
 						resolve(addresses[0]);
 					}
